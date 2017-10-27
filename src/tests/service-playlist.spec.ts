e69a437f908afad853ef6e6f0db65f0edce5239c
Stas <staskgt@gmail.com>
2017-10-27 18:17:42 +0300

    feat: add playlist tests

--- src/tests/service-playlist.spec.ts
+++ src/tests/service-playlist.spec.ts
@@ -3,6 +3,10 @@ import { KalturaBrowserHttpClient } from "../kaltura-clients/kaltura-browser-htt
 import { PlaylistListAction } from "../types/PlaylistListAction";
 import { KalturaPlaylistListResponse } from "../types/KalturaPlaylistListResponse";
 import { KalturaPlaylist } from "../types/KalturaPlaylist";
+import { KalturaPlaylistType } from '../types/KalturaPlaylistType';
+import { PlaylistAddAction } from '../types/PlaylistAddAction';
+import { PlaylistDeleteAction } from '../types/PlaylistDeleteAction';
+import { PlaylistUpdateAction } from '../types/PlaylistUpdateAction';
 
 describe(`service "Playlist" tests`, () => {
   let kalturaClient: KalturaBrowserHttpClient = null;
@@ -40,5 +44,60 @@ describe(`service "Playlist" tests`, () => {
         done();
       }
     );
+  });
+
+  test(`invoke "createRemote:staticList" action`, () => {
+    const playlist = new KalturaPlaylist({
+      name: "tstest.PlaylistTests.test_createRemote",
+      playlistType: KalturaPlaylistType.staticList
+    });
+    kalturaClient.request(new PlaylistAddAction({ playlist }))
+      .then(
+        (response) => {
+          expect(response instanceof KalturaPlaylist).toBeTruthy();
+          expect(typeof response.id).toBe("string");
+          kalturaClient.request(new PlaylistDeleteAction({ id: response.id }));
+        },
+        () => {
+          fail(`failed to perform request`);
+        }
+      );
+  });
+
+  test(`invoke "update" action`, () => {
+    const playlist = new KalturaPlaylist({
+      name: "tstest.PlaylistTests.test_createRemote",
+      referenceId: "tstest.PlaylistTests.test_update",
+      playlistType: KalturaPlaylistType.staticList
+    });
+    kalturaClient.request(new PlaylistAddAction({ playlist }))
+      .then(({ id }) => {
+          playlist.name = "Changed!";
+          return kalturaClient.request(new PlaylistUpdateAction({ id, playlist }));
+        }
+      )
+      .then(({ id, name }) => {
+        expect(name).toBe("Changed!");
+        kalturaClient.request(new PlaylistDeleteAction({ id }));
+      })
+      .catch(() => fail(`failed to perform request`));
+  });
+
+  test(`invoke "createRemote:dynamicList" action`, () => {
+    const playlist = new KalturaPlaylist({
+      name: "tstest.PlaylistTests.test_createRemote",
+      playlistType: KalturaPlaylistType.dynamic
+    });
+    kalturaClient.request(new PlaylistAddAction({ playlist }))
+      .then(
+        (response) => {
+          expect(response instanceof KalturaPlaylist).toBeTruthy();
+          expect(typeof response.id).toBe("string");
+          kalturaClient.request(new PlaylistDeleteAction({ id: response.id }));
+        },
+        () => {
+          fail(`failed to perform request`);
+        }
+      );
   });
 });
