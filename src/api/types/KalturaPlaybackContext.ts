a566f2d77ffbfb445a29d9061a253f79d7c8a1f2
Kaltura CSI <community@kaltura.com>
2018-12-27 03:20:03 -0500

    New clientlib ver

--- src/api/types/KalturaPlaybackContext.ts
+++ src/api/types/KalturaPlaybackContext.ts
@@ -2,6 +2,7 @@
 import { KalturaObjectMetadata } from '../kaltura-object-base';
 import { KalturaTypesFactory } from '../kaltura-types-factory';
 import { KalturaPlaybackSource } from './KalturaPlaybackSource';
+import { KalturaCaptionPlaybackPluginData } from './KalturaCaptionPlaybackPluginData';
 import { KalturaFlavorAsset } from './KalturaFlavorAsset';
 import { KalturaRuleAction } from './KalturaRuleAction';
 import { KalturaAccessControlMessage } from './KalturaAccessControlMessage';
@@ -9,6 +10,7 @@ import { KalturaObjectBase, KalturaObjectBaseArgs } from '../kaltura-object-base
 
 export interface KalturaPlaybackContextArgs  extends KalturaObjectBaseArgs {
     sources? : KalturaPlaybackSource[];
+	playbackCaptions? : KalturaCaptionPlaybackPluginData[];
 	flavorAssets? : KalturaFlavorAsset[];
 	actions? : KalturaRuleAction[];
 	messages? : KalturaAccessControlMessage[];
@@ -18,6 +20,7 @@ export interface KalturaPlaybackContextArgs  extends KalturaObjectBaseArgs {
 export class KalturaPlaybackContext extends KalturaObjectBase {
 
     sources : KalturaPlaybackSource[];
+	playbackCaptions : KalturaCaptionPlaybackPluginData[];
 	flavorAssets : KalturaFlavorAsset[];
 	actions : KalturaRuleAction[];
 	messages : KalturaAccessControlMessage[];
@@ -26,6 +29,7 @@ export class KalturaPlaybackContext extends KalturaObjectBase {
     {
         super(data);
         if (typeof this.sources === 'undefined') this.sources = [];
+		if (typeof this.playbackCaptions === 'undefined') this.playbackCaptions = [];
 		if (typeof this.flavorAssets === 'undefined') this.flavorAssets = [];
 		if (typeof this.actions === 'undefined') this.actions = [];
 		if (typeof this.messages === 'undefined') this.messages = [];
@@ -39,6 +43,7 @@ export class KalturaPlaybackContext extends KalturaObjectBase {
             {
                 objectType : { type : 'c', default : 'KalturaPlaybackContext' },
 				sources : { type : 'a', subTypeConstructor : KalturaPlaybackSource, subType : 'KalturaPlaybackSource' },
+				playbackCaptions : { type : 'a', subTypeConstructor : KalturaCaptionPlaybackPluginData, subType : 'KalturaCaptionPlaybackPluginData' },
 				flavorAssets : { type : 'a', subTypeConstructor : KalturaFlavorAsset, subType : 'KalturaFlavorAsset' },
 				actions : { type : 'a', subTypeConstructor : KalturaRuleAction, subType : 'KalturaRuleAction' },
 				messages : { type : 'a', subTypeConstructor : KalturaAccessControlMessage, subType : 'KalturaAccessControlMessage' }
