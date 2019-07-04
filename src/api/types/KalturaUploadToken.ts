1d53cd12e1dcfa2f70431f42f5b6d8dc4cc7cb79
Kaltura CSI <community@kaltura.com>
2019-07-04 01:24:20 -0400

    New clientlib ver

--- src/api/types/KalturaUploadToken.ts
+++ src/api/types/KalturaUploadToken.ts
@@ -9,7 +9,6 @@ export interface KalturaUploadTokenArgs  extends KalturaObjectBaseArgs {
     fileName? : string;
 	fileSize? : number;
 	autoFinalize? : KalturaNullableBoolean;
-	minimumChunkSize? : number;
 }
 
 
@@ -26,7 +25,6 @@ export class KalturaUploadToken extends KalturaObjectBase {
 	readonly updatedAt : Date;
 	readonly uploadUrl : string;
 	autoFinalize : KalturaNullableBoolean;
-	minimumChunkSize : number;
 
     constructor(data? : KalturaUploadTokenArgs)
     {
@@ -50,8 +48,7 @@ export class KalturaUploadToken extends KalturaObjectBase {
 				createdAt : { type : 'd', readOnly : true },
 				updatedAt : { type : 'd', readOnly : true },
 				uploadUrl : { type : 's', readOnly : true },
-				autoFinalize : { type : 'en', subTypeConstructor : KalturaNullableBoolean, subType : 'KalturaNullableBoolean' },
-				minimumChunkSize : { type : 'n' }
+				autoFinalize : { type : 'en', subTypeConstructor : KalturaNullableBoolean, subType : 'KalturaNullableBoolean' }
             }
         );
         return result;
