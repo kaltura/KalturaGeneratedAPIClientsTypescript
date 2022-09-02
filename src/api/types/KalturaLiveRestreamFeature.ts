ea9e5d9a50add99d460b6db5422e7f0e99ef911b
Kaltura CSI <community@kaltura.com>
2022-09-01 20:32:44 -0400

    New clientlib ver

---
+++ src/api/types/KalturaLiveRestreamFeature.ts
@@ -0,0 +1,40 @@
+
+import { KalturaObjectMetadata } from '../kaltura-object-base';
+import { KalturaTypesFactory } from '../kaltura-types-factory';
+import { KalturaLiveFeature, KalturaLiveFeatureArgs } from './KalturaLiveFeature';
+
+export interface KalturaLiveRestreamFeatureArgs  extends KalturaLiveFeatureArgs {
+    primaryUrl? : string;
+	secondaryUrl? : string;
+	streamKey? : string;
+}
+
+
+export class KalturaLiveRestreamFeature extends KalturaLiveFeature {
+
+    primaryUrl : string;
+	secondaryUrl : string;
+	streamKey : string;
+
+    constructor(data? : KalturaLiveRestreamFeatureArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaLiveRestreamFeature' },
+				primaryUrl : { type : 's' },
+				secondaryUrl : { type : 's' },
+				streamKey : { type : 's' }
+            }
+        );
+        return result;
+    }
+}
+
+KalturaTypesFactory.registerType('KalturaLiveRestreamFeature',KalturaLiveRestreamFeature);
